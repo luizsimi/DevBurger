89ac389d156f9594ea8f9ce8407ce2cd15d266ef
luizsimi <luizsimi455@gmail.com>
2024-10-25 03:21:20 -0300

    Finalizando Projeto

--- script.js
+++ script.js
@@ -134,12 +134,46 @@ addressInput.addEventListener("input", function(event){
 
 //Finalizar pedido
 checkoutBtn.addEventListener("click", function(){
+  
+  const isOpen = checkRestaurantOpen();
+  if(!isOpen){
+   
+    Toastify({
+        text: "Ops o restaurante está fechado!",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#ef4444",
+        },
+    }).showToast();
+
+   return;
+  }
+
   if(cart.length === 0) return;
   if(addressInput.value === ""){
     addressWarn.classList.remove("hidden")
     addressInput.classList.add("border-red-500")
     return;
   }
+
+   //Enviar o pedido para api WhatsApp
+   const cartItems = cart.map((item) =>{
+    return (
+      ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} |`
+    )
+   }).join("")
+
+   const message = encodeURIComponent(cartItems)
+   const phone = "19996816200"
+
+   window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+   cart = [];
+   updateCartModal();
 
 })
 
